ad75d882492a53a8b6d2e52f845d0c7a13b204de
nglgzz <angelo@nglgzz.com>
2017-09-02 21:12:18 +0200

    forgot to uncomment a few things

--- scripts/updateES.sh
+++ scripts/updateES.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 node scripts/parseMaps.js
-# rm -r maps
+rm -r maps
 
 if [[ -f scripts/utils/.last-update ]]; then
   # Get all maps that changed since last uploaded commit.
+  # TODO - update so on dev environment it doesn't fetch
+  # the git hash from the .last-update file
   maps=($(
     git diff --name-only $(cat scripts/utils/.last-update) HEAD |
       grep .json |
@@ -18,13 +20,13 @@ else
 fi
 
 # # Loop all maps and upload to ES.
-# for map in ${maps[@]}; do
-#   node scripts/updateMap.js $map
-# done
+for map in ${maps[@]}; do
+  node scripts/updateMap.js $map
+done
 
 # Write hash of last commit
-# if [[ $NODE_ENV = production ]]; then
-#   echo $(git rev-parse HEAD) > scripts/utils/.last-update
-# fi
+if [[ $NODE_ENV = production ]]; then
+  echo $(git rev-parse HEAD) > scripts/utils/.last-update
+fi
 
 echo 'Done.'
